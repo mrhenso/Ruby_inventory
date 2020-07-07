5e5871d5fe7bee943393e73d5bf58cc1f4c3ec52
Matthew <Matt@Matthews-MacBook-Pro.local>
2020-07-06 19:12:30 -0500

    Inventory Questionaire

--- git_car.rb
+++ git_car.rb
@@ -1,2 +1,12 @@
-puts "The time is #{Time.new}"
-puts "garbage"
+puts "Year?"
+input1 = gets
+puts "Make?"
+input2 = gets
+puts "Model?"
+input3 = gets
+puts "Color?"
+input = gets
+puts "Doors?"
+input4 = gets
+
+puts "\n\nUsed Car Inventory: \nWe have the following for sale \n #{input1} #{input2} #{input3} #{input4} for $10000"
