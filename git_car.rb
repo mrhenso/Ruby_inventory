bede483dee70271ec2ab0ea5d16aa8fdcab77fc0
Matthew <Matt@Matthews-MacBook-Pro.local>
2020-07-09 20:07:55 -0500

    add space

--- git_car.rb
+++ git_car.rb
@@ -2,11 +2,14 @@ def ask(input)
  puts "What the #{input.downcase} of your car?"
  gets
     end
+
 year = ask("Year")
 make = ask("Make")
 model = ask("Model")
 mileage = ask("Mileage")
 color = ask("Color")
 price = ask("Price")
+
 puts ""
+
 puts "Vehicle is a #{year.chomp} #{make.capitalize.chomp} #{model.capitalize.chomp} that is #{color.chomp} in color with #{mileage.chomp} for $#{price.chomp}."
