71ca58a3e9b7a751d05f789201e58d91209a39da
Matthew <Matt@Matthews-MacBook-Pro.local>
2020-07-09 20:06:32 -0500

    cleaned up code

--- git_car.rb
+++ git_car.rb
@@ -1,49 +1,5 @@
-# # puts "Year?"
-# # input1 = gets.chomp
-# # puts "Make?"
-# # input2 = gets.chomp
-# # puts "Model?"
-# # input3 = gets.chomp
-# # puts "Color?"
-# # input = gets.chomp
-# # puts "Doors?"
-# # input4 = gets.chomp
-# # puts "Price?"
-# # input5 = gets.chomp
-# # puts "\n\nUsed Car Inventory: \nWe have the following for sale \n\n#{input1} #{input2} #{input3} #{input4} for $#{input5}"
-
-  
-
-
-# # def info_form 
- 
-# #     make = gets.chomp
-# #     year = gets.chomp
-# #     model = gets.chomp
-# #     mileage = gets.chomp
-# #     price = gets.chomp
-# #     "We have a #{year} #{make.capitalize} #{model.capitalize} with only #{mileage} miles for $#{price}!!!"
-# # end
-
-# # puts info_form
-
-# def questionaire
-#     puts "What Year?" 
-#     year = gets.chomp
-#     puts "\nMake?"
-#     make = gets.chomp
-#     puts "\nModel?"
-#     model = gets.chomp
-#     puts "\nMileage?"
-#     mileage = gets.chomp
-#     puts "\nPrice?"
-#     price = gets.chomp
-#     puts "\nYour vehicle is a #{year} #{make.capitalize} #{model.capitalize} with #{mileage} miles for $#{price}, is that right?"
-    
-# end
-# puts questionaire
 def ask(input)
- puts "What #{input.downcase} is your car?"
+ puts "What the #{input.downcase} of your car?"
  gets
     end
 year = ask("Year")
@@ -52,4 +8,5 @@ model = ask("Model")
 mileage = ask("Mileage")
 color = ask("Color")
 price = ask("Price")
+puts ""
 puts "Vehicle is a #{year.chomp} #{make.capitalize.chomp} #{model.capitalize.chomp} that is #{color.chomp} in color with #{mileage.chomp} for $#{price.chomp}."
