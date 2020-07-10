651ee99197c27fb61e0e80fc680953f0fe17000d
Matthew <Matt@Matthews-MacBook-Pro.local>
2020-07-09 20:01:57 -0500

    added questions in method, added detail in string

--- git_car.rb
+++ git_car.rb
@@ -43,12 +43,13 @@
 # end
 # puts questionaire
 def ask(input)
- puts "What #{input} is your car?"
+ puts "What #{input.downcase} is your car?"
  gets
     end
 year = ask("Year")
 make = ask("Make")
 model = ask("Model")
 mileage = ask("Mileage")
+color = ask("Color")
 price = ask("Price")
-puts "#{year.chomp} #{make.capitalize.chomp} #{model.capitalize.chomp} #{mileage.chomp} $#{price.chomp}"
+puts "Vehicle is a #{year.chomp} #{make.capitalize.chomp} #{model.capitalize.chomp} that is #{color.chomp} in color with #{mileage.chomp} for $#{price.chomp}."
