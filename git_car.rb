992b34b8eb20c4a104525c7997c2fca5cec2597f
Matthew <Matt@Matthews-MacBook-Pro.local>
2020-07-12 21:23:25 -0500

    add space

--- git_car.rb
+++ git_car.rb
@@ -12,7 +12,6 @@ price = ask("price")
 drivetrain = ask("drivetrain")
 transmission = ask("transmission")
 
-
 puts ""
 
 puts "Vehicle is a #{year.chomp} #{make.capitalize.chomp} #{model.capitalize.chomp} that is #{color.chomp} in color with #{mileage.chomp} for $#{price.chomp}."
